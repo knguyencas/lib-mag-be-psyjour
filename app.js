771d82c5a6a31c358d330bdd18fb4ffefe188cd5
knguyencas <168413274+knguyencas@users.noreply.github.com>
2025-12-12 08:42:10 +0700

    add static file serving and update index.html for improved API documentation

--- app.js
+++ app.js
@@ -76,6 +76,11 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
